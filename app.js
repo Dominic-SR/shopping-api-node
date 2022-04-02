0fba0698e40d2ee54fe3b2e48e990b71d94deb3a
Dominic <dominicseduraja3@gmail.com>
2022-04-02 23:54:33 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require('morgan');
+var indexRouter = require('./routes/index.routes');
+const cors = require('cors');
+var cookieParser = require('cookie-parser');
+const passport = require('passport')
+
+const app = express();
+app.use(express.json({limit: '50mb'}));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+const { ExtractJwt, Strategy } = require('passport-jwt');
+
+app.listen(5000,()=>{
+    console.log("Port Run Localhost:3421");
+})
